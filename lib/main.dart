63fb5d7924d67c72564d21dbac2f3363fb63241d
Nikhil Pareek <nikhilpareekpandit@gmail.com>
2025-04-24 02:24:36 +0530

    setting up web demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_app/weather_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
+
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
@@ -11,6 +18,10 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      // ignore: deprecated_member_use
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: const WeatherScreen(),
